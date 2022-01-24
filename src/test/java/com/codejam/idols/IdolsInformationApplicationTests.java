410f6cc79ffb065524c70c446647871c1298f76a
nagarjuna9722 <98144057+nagarjuna9722@users.noreply.github.com>
2022-01-24 17:32:09 +0530

    Update IdolsInformationApplicationTests.java

--- src/test/java/com/codejam/idols/IdolsInformationApplicationTests.java
+++ src/test/java/com/codejam/idols/IdolsInformationApplicationTests.java
@@ -3,6 +3,9 @@ package com.codejam.idols;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -31,7 +34,36 @@ public class IdolsInformationApplicationTests {
 		personalInformation.setRealName("malisiya");
 		when(personalInfoRepository.save(personalInformation)).thenReturn(personalInformation);
 		Integer save = personalInfoService.savePersonalInfo(personalInformation);
+		assertThat(personalInformation.getIdolStatus().equalsIgnoreCase("ACTIVE"));
 		assertThat(save).isNotNull();
+
+	}
+
+	@Test
+	public void test_getAllUsersPersonalInformations() {
+		List<PersonalInformation> personalInformationList = new ArrayList<>();
+		PersonalInformation personalInformation = new PersonalInformation();
+		personalInformation.setIdolName("kul");
+		personalInformation.setIdolStatus("active");
+		personalInformation.setAddress("hyd");
+		personalInformation.setRealName("malisiya");
+		personalInformationList.add(personalInformation);
+		when(personalInfoRepository.findAll()).thenReturn(personalInformationList);
+		List<PersonalInformation> list = personalInfoService.getAllUsersPersonalInformations();
+		assertThat(list).isNotEmpty();
+
+	}
+
+	@Test
+	public void test_fetchRandomIdolPersonalInformation() {
+		PersonalInformation personalInformation = new PersonalInformation();
+		personalInformation.setIdolName("remy");
+		personalInformation.setIdolStatus("ACTIVE");
+		personalInformation.setAddress("Malaysia");
+		personalInformation.setRealName("rahimi");
+		when(personalInfoRepository.fetchRandomIdolPersonalInformation()).thenReturn(personalInformation.toString());
+		String info = personalInfoService.fetchRandomIdolPersonalInformation();
+		assertThat(info).isNotNull();
 
 	}
 
